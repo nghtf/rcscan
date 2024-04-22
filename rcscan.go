a44217f0a491681a51845915094bcdd6e6ce0f11
roman krasnov <krasnov.roman@gmail.com>
2024-04-22 15:13:05 +0300

    package description added

--- rcscan.go
+++ rcscan.go
@@ -1,3 +1,37 @@
+/*
+Package provides minimalistic access to parameters stored in .rc-like configuration files.
+
+Example for getmailrc cnfiguration file:
+
+	[retriever]
+	type = SimplePOP3SSLRetriever
+	server = pop.domain.example
+	username = user@domain.example
+	password = P@$$w0rd
+
+	[destination]
+	type = Maildir
+	path = ./getmail/maildir/
+
+	[options]
+	delete = false
+	message_log = ./getmail/log
+
+With rscan you can easily access any parameter inside the configuration.
+Let's pull "path" parameter from "destination" section:
+
+	rc, err := rcscan.New("./path-to/getmailrc")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		path, err := rc.GetParam("destination", "path")
+		if err != nil {
+			log.Debug("problem")
+		}
+		fmt.Println(path)
+	}
+	
+*/
 package rcscan
 
 import (
